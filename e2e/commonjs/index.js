210e8ec69ce7592604b30d470c1f76815868fdff
levg34 <levg34@users.noreply.github.com>
2023-10-18 19:20:20 +0000

    create e2e tests for commonjs

---
+++ e2e/commonjs/index.js
@@ -0,0 +1,36 @@
+const ORM = require('typescript-nedb-orm').ORM
+
+/**
+ * @typedef {Object} IPerson
+ * @property {string} name
+ * @property {string} email
+ */
+
+class Person extends ORM {
+    /**
+     * @param {IPerson} person
+     */
+    constructor(person) {
+        super(person)
+        this.name = person.name
+        this.email = person.email
+    }
+}
+
+async function main() {
+    const person = new Person({ name: 'Alice', email: 'alice@example.com' })
+
+    const savedPerson = await person.save()
+
+    const persons = await Person.find()
+
+    const updated = await Person.update({ name: 'Alice' }, { email: 'alice@new.com' })
+
+    await new Person(savedPerson).delete()
+
+    console.log('Saved person:', savedPerson)
+    console.log('All persons:', persons)
+    console.log('Updated persons:', updated)
+}
+
+main()
